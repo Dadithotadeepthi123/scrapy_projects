dd79245389f41176d2e609a91e3bcb7611f9106d
Dadithota Deepthi <157580130+Dadithotadeepthi123@users.noreply.github.com>
2024-09-14 19:16:04 +0530

    Add files via upload
    Scrapy XPath Crawler:
    This project demonstrates how to use Scrapy, a powerful web scraping framework in Python, to crawl a sample website using XPath selectors exclusively. This script is designed to extract specific data from a web page, leveraging XPath expressions to navigate and select elements in the HTML structure.
    Features:
    Utilizes Scrapy framework for web scraping.
    Focuses on XPath selectors to extract data.
    Example script is tailored to a sample website provided in the Scrapy documentation.
    Usage:
    Install Scrapy using pip install scrapy.
    Save the provided script as xpath_crawler.py.
    Run the script using the Scrapy command: scrapy runspider xpath_crawler.py.

---
+++ output_script.py
@@ -0,0 +1,7 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/deept/Downloads/scrapy_projects/script.py
+Image Links: ['image1.html', 'image2.html', 'image3.html', 'image4.html', 'image5.html']
+Image Names: ['Name: My image 1 ', 'Name: My image 2 ', 'Name: My image 3 ', 'Name: My image 4 ', 'Name: My image 5 ']
+Thumbnail Links: ['image1_thumb.jpg', 'image2_thumb.jpg', 'image3_thumb.jpg', 'image4_thumb.jpg', 'image5_thumb.jpg']
---
+++ xpath.py
@@ -0,0 +1,41 @@
+from lxml import html
+
+# Sample HTML content
+html_content = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <base href='http://example.com/' />
+    <title>Example website</title>
+  </head>
+  <body>
+    <div id='images'>
+      <a href='image1.html'>Name: My image 1 <br /><img src='image1_thumb.jpg' alt='image1'/></a>
+      <a href='image2.html'>Name: My image 2 <br /><img src='image2_thumb.jpg' alt='image2'/></a>
+      <a href='image3.html'>Name: My image 3 <br /><img src='image3_thumb.jpg' alt='image3'/></a>
+      <a href='image4.html'>Name: My image 4 <br /><img src='image4_thumb.jpg' alt='image4'/></a>
+      <a href='image5.html'>Name: My image 5 <br /><img src='image5_thumb.jpg' alt='image5'/></a>
+    </div>
+  </body>
+</html>
+'''
+
+
+# Parse the HTML content
+tree = html.fromstring(html_content)
+
+# Extract all image URLs (the href attribute of the <a> tags)
+image_links = tree.xpath('//div[@id="images"]/a/@href')
+print("Image Links:", image_links)
+
+# Extract the names of the images (text inside <a> tags)
+image_names = tree.xpath('//div[@id="images"]/a/text()[1]')
+print("Image Names:", image_names)
+
+# Extract the thumbnail URLs (the src attribute of the <img> tags)
+thumbnail_links = tree.xpath('//div[@id="images"]/a/img/@src')
+print("Thumbnail Links:", thumbnail_links)
+
+
+
+
